4c5a317f7ed2ef72aae7cec3a7fb4312e1f2eaea
Sushmita Thapa <your-sushmitat186@gmail.com>
2026-06-07 00:39:47 +0900

    change done

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
-const images = [
-  {
-    src: "images/20250807_161951.jpg",
-    alt: "My Photo"
-  },
-  {
-    src: "images/20240402_135812.jpg",
-    alt: "Tree"
-  },
-  {
-    src: "images/20260202_123213.jpg",
-    alt: "Temple"
-  },
-  {
-    src: "images/6885163461946573829_avatar.png (1).jpg",
-    alt: "Sunset"
-  },
-  {
-    src: "images/IMG-20260403-WA0092.jpg",
-    alt: "Seoul Building"
-  },
-  {
-    src: "images/received_2948728571962084.jpeg",
-    alt: "Waterfall"
-  }
-];
-
-let currentIndex = 0;
-
-const galleryImage = document.getElementById("galleryImage");
-const nextBtn = document.getElementById("nextBtn");
-const darkModeBtn = document.getElementById("darkModeBtn");
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % images.length;
-
-  galleryImage.src = images[currentIndex].src;
-  galleryImage.alt = images[currentIndex].alt;
+// DARK MODE TOGGLE
+document.getElementById("darkModeBtn").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
 });
 
-darkModeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+// IMAGE SLIDER
+const images = [
+    "images/20250807_161951.jpg",
+    "images/20250807_161952.jpg",
+    "images/20250807_161953.jpg"
+];
 
-  if (document.body.classList.contains("dark-mode")) {
-    darkModeBtn.textContent = "☀️ Light Mode";
-  } else {
-    darkModeBtn.textContent = "🌙 Dark Mode";
-  }
+let index = 0;
+
+document.getElementById("nextBtn").addEventListener("click", function () {
+    index = (index + 1) % images.length;
+    document.getElementById("galleryImage").src = images[index];
 });
