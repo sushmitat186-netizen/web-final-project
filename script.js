e5d50c487a60f75a142a2783542570685b9592eb
Sushmita Thapa <your-sushmitat186@gmail.com>
2026-06-07 14:01:50 +0900

    changes

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 const images = [
-  {
-    src: "images/20250807_161951.jpg",
-    alt: "My Photo"
-  },
-  {
-    src: "images/20240402_135812.jpg",
-    alt: "Tree"
-  },
-  {
-    src: "images/20260202_123213.jpg",
-    alt: "Temple"
-  },
-  {
-    src: "images/6885163461946573829_avatar.png (1).jpg",
-    alt: "Sunset"
-  },
-  {
-    src: "images/IMG-20260403-WA0092.jpg",
-    alt: "Seoul Building"
-  },
-  {
-    src: "images/received_2948728571962084.jpeg",
-    alt: "Waterfall"
-  }
+    {
+        src: "images/20250807_161951.jpg",
+        alt: "Profile Photo"
+    },
+    {
+        src: "images/20240402_135812.jpg",
+        alt: "Tree"
+    },
+    {
+        src: "images/20260202_123213.jpg",
+        alt: "Temple"
+    },
+    {
+        src: "images/IMG-20260403-WA0092.jpg",
+        alt: "Seoul Building"
+    },
+    {
+        src: "images/received_2948728571962084.jpeg",
+        alt: "Waterfall"
+    }
 ];
 
 let currentIndex = 0;
@@ -31,19 +27,25 @@ const galleryImage = document.getElementById("galleryImage");
 const nextBtn = document.getElementById("nextBtn");
 const darkModeBtn = document.getElementById("darkModeBtn");
 
+/* Next Gallery Image */
 nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % images.length;
+    currentIndex++;
 
-  galleryImage.src = images[currentIndex].src;
-  galleryImage.alt = images[currentIndex].alt;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    galleryImage.src = images[currentIndex].src;
+    galleryImage.alt = images[currentIndex].alt;
 });
 
+/* Dark Mode Toggle */
 darkModeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
 
-  if (document.body.classList.contains("dark-mode")) {
-    darkModeBtn.textContent = "☀️ Light Mode";
-  } else {
-    darkModeBtn.textContent = "🌙 Dark Mode";
-  }
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeBtn.textContent = "☀️ Light Mode";
+    } else {
+        darkModeBtn.textContent = "🌙 Dark Mode";
+    }
 });
